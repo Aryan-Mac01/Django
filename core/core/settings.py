072c1948dbcda22660a3ed76d5ee5fc766e52e12
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-03-20 22:53:56 +0530

    home and account setup

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
+
+EXTERNAL_APPS = [
+    'accounts',
+    'home'
+]
+
+INSTALLED_APPS += EXTERNAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
