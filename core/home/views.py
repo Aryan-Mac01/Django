be901863567b62459f6145747b563ad0b16e4f2d
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-03-23 23:33:21 +0530

    Truncate

--- core/home/views.py
+++ core/home/views.py
@@ -10,7 +10,12 @@ def home(request):
         {'name': 'Pragati', 'age': 17}
 
     ] 
-    return render(request, "home/index.html", context = {'peoples' : peoples})
+
+    text = ""
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+    
+
+    return render(request, "home/index.html", context = {'peoples' : peoples, 'text' :text })
 
 def success_page(request):
     return HttpResponse("<h1>Hey this is a success page</h1>")
