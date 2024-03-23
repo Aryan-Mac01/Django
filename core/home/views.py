ba92b41f036e389fea8158cff9d222becf2222da
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-03-24 00:52:20 +0530

    contact and about

--- core/home/views.py
+++ core/home/views.py
@@ -11,11 +11,11 @@ def home(request):
 
     ] 
 
-    text = ""
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+    vegetables = ['Pumpkin', 'Tomato', 'Potato']
+    
     
 
-    return render(request, "home/index.html", context = {'peoples' : peoples, 'text' :text })
+    return render(request, "home/index.html", context = {'peoples' : peoples, 'vegetables' : vegetables })
 
 def success_page(request):
     return HttpResponse("<h1>Hey this is a success page</h1>")
