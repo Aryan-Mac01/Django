f318383e10b1b5edf4fed6c918d18ff60ed3d889
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-03-27 21:42:40 +0200

    models

--- core/home/models.py
+++ core/home/models.py
@@ -3,9 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
+    #id = models.AutoField() --> ye default field hoti jo django khud se automatic add kardeta
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    email = models.EmailField()
+    email = models.EmailField(null = True, blank = True)
     address = models.TextField()
     image = models.ImageField()
     file = models.FileField()
+    document = models.FileField()
+
+
+class Product(models.Model):
+    pass
