534cabd246a21d893440e8b6fa65cafba16294da
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-04-13 00:43:20 +0530

    frontend to backend

--- core/vege/views.py
+++ core/vege/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
 # Create your views here.
 
 def receipes(request):
+    if request.method == "POST":
+        data = request.POST
+        receipe_image = request.FILES.get('receipe_image')
+        receipe_name = data.get('receipe_name')
+        receipe_description = data.get('receipe_description')
+
+        Receipe.objects.create(
+            receipe_image = receipe_image,
+            receipe_name = receipe_name,
+            receipe_description = receipe_description
+        )
+    
+    return redirect('/receipes/')
+
     return render(request, 'receipes.html')
